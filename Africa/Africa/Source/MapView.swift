632d76bcd565b07f4b58839cb0b9bb40eec1c5ab
Matheus Evangelista <matheus.evangelista@g.globo>
2022-10-11 19:27:40 -0300

    Setup project and create the initial structure

---
+++ Africa/Africa/Source/MapView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
