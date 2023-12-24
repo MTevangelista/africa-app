152ef732f4eaa84126670ebb192cbda96f7eea8b
Matheus Evangelista <matheus.evangelista@g.globo>
2023-12-23 19:36:29 -0500

    Develop a complex map using mapkit with custom annotation

---
+++ Africa/Africa/Source/Model/LocationModel.swift
@@ -0,0 +1,13 @@
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
